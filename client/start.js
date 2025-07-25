031d3cf27ca3c7b9a19df13e76de3e4f2f907b96
camj18 <camj18@bu.edu>
2025-07-24 18:17:36 -0700

    chore(client): add start script

---
+++ client/start.js
@@ -0,0 +1,4 @@
+// This script is a placeholder for a client‑side development server.
+// In this scaffold, the root‑level server serves the client build.
+// Running this script directly will simply log instructions for developers.
+console.log('Please run "npm start" from the project root to start the server.');
